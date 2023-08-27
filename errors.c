246e2b852b84f6d6b69d67548394b9cd59aadfd6
JuliusDgenius <ibejulius1@gmail.com>
2023-08-27 06:57:35 +0100

    Handle error messages

---
+++ errors.c
@@ -0,0 +1,44 @@
+#include "monty.h"
+
+/**
+ * exit_error - prints exit value on failure
+ *
+ * Return: void
+ */
+void exit_error()
+{
+    exit(EXIT_FAILURE);
+}
+
+/**
+ * usage_error - Prints usage error if program is used wrongly
+ *
+ * Return: void
+ */
+void usage_error()
+{
+    if (argc != 2)
+    {
+        fprintf(stderr, "USAGE: monty file\n");
+    }
+}
+
+/**
+ * open_error - Display an error message if file cannot be open and exit
+ *
+ * Return: Nothing
+ */
+void open_error()
+{
+    fprintf(stderr, "Error: Can't open file <file>\n", argv[1]);
+}
+
+/**
+ * unknown_error - prints unknown error to stderr if an invalid opcode is entered
+ *
+ * Return: returns nothing
+ */
+void unknown_error()
+{
+    fprintf(stderr, "L%d: unknown instruction %s\n", line_count, line);
+}
